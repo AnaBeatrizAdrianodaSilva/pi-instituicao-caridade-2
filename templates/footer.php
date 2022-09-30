7a5280d8d21fcad1812ee0fb8d262c31d834d2f5
Ana Beatriz Adriano da Silva <ana.github0@gmail.com>
2022-09-30 10:47:42 -0300

    ."

--- templates/footer.php
+++ templates/footer.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
     <!-- favicon -->
-    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../img2/favicon.ico" type="image/x-icon">
 
     <!-- CSS -->
     <link rel="stylesheet" href="../CSS/styleFooter.css">
@@ -14,23 +14,18 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"> 
-        <!-- font-family: 'Ubuntu', sans-serif; -->
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-        <!-- font-family: 'Comfortaa', cursive; -->
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <!-- font-family: 'Mulish', sans-serif; -->
 </head>
 <body>
-    <footer class="rodape">
-        <div class="contato">
-            <h1>© Copyright Senac Joinville Técnico INFO</h1>
-            
+    <footer class='rodape'>
+        <svg width="1864" height="395" viewBox="0 0 1864 395" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 177.738L34.5617 152.597C68.9939 127.833 138.506 76.9867 207.111 77.2692C276.105 76.9867 345.617 127.833 414.222 132.541C483.216 137.249 552.728 97.7019 621.333 67.2883C690.327 37.4396 759.839 16.7245 828.444 7.026C897.438 -3.0491 966.95 -3.04908 1035.56 12.0165C1104.55 27.0821 1174.06 57.2132 1242.67 92.3348C1311.66 127.833 1381.17 167.38 1449.78 187.813C1518.77 207.869 1588.28 207.869 1656.89 192.803C1725.88 177.738 1795.39 147.607 1829.05 132.541L1864 117.475V238H1829.44C1795.01 238 1725.49 238 1656.89 238C1587.9 238 1518.38 238 1449.78 238C1380.78 238 1311.27 238 1242.67 238C1173.67 238 1104.16 238 1035.56 238C966.562 238 897.05 238 828.444 238C759.451 238 689.939 238 621.333 238C552.339 238 482.828 238 414.222 238C345.228 238 275.717 238 207.111 238C138.117 238 68.6056 238 34.95 238H0V177.738Z" fill="#262DDA"/>
+            <rect y="238" width="1864" height="157" fill="#262DDA"/>
+        </svg>
 
-        </div>
-        
-       
+        <p>&copy; Copyright Senac Joinville Técnico INFO</p>
+
     </footer>
 </body>
 </html>
