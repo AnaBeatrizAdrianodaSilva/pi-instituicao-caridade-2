a6cb9e8477e176fbb1c44f40a799f95aac347f2b
Joao Gabriel <konestiqui@gmail.com>
2022-09-30 10:50:31 -0300

    mudei o index

--- paginas/index.php
+++ paginas/index.php
@@ -20,15 +20,6 @@
 <body>
     <!-- hero section start -->
     <div class="hero" id="back">
-    <header class='cabeca'>
-    <?xml version="1.0" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class='svg'><path fill="#262DDA" fill-opacity="1" d="M0,64L26.7,90.7C53.3,117,107,171,160,170.7C213.3,171,267,117,320,112C373.3,107,427,149,480,181.3C533.3,213,587,235,640,245.3C693.3,256,747,256,800,240C853.3,224,907,192,960,154.7C1013.3,117,1067,75,1120,53.3C1173.3,32,1227,32,1280,48C1333.3,64,1387,96,1413,112L1440,128L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"></path></svg>
-    <!-- 0 0 1440 320 -->
-    <nav>
-        <a href="#">Sobre</a>
-        <img src="../img/logo.png" alt="Logo">
-        <a href="#">Desenvolvedores</a>
-    </nav>
-</header>
         <div class="content">
 
         </div>
@@ -70,39 +61,6 @@
     </section>
 
 
-
-
-    <!-- Contact Me -->
-    <div class="contact-me">
-        <p>Vou fazer lindos Web sites para vocês.</p>
-        <a class="button-two" href="#">Contrate-me</a>
-    </div>
-
-    <!-- footer start -->
-    <!-- <footer id="footer">
-        <p>João Gabriel Lemos Alves</p>
-        <p>Para mais informações envie alguma mensagem ao email joao.gabriel1@alunos.sc.senac.br</p>
-        <div class="social">
-            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-            <a href="#"><i class="fa-brands fa-github"></i></a>
-            <a href="#"><i class="fa-brands fa-instagram"></i></a>
-        </div>
-        <p class="end">Direitos reservados a João Gabriel</p>
-    </footer>  -->
-    <footer class="rodape">
-        
-        <div class="contato">
-
-
-            <h1>© Copyright Senac Joinville Técnico INFO</h1>
-
-
-        </div>
-        <svg width="1648" height="238" viewBox="0 0 1648 238" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M0 177.738L30.5567 152.597C60.9989 127.833 122.456 76.9867 183.111 77.2692C244.11 76.9867 305.567 127.833 366.222 132.541C427.221 137.249 488.678 97.7019 549.333 67.2883C610.332 37.4396 671.789 16.7245 732.444 7.026C793.443 -3.0491 854.9 -3.04907 915.556 12.0165C976.554 27.0821 1038.01 57.2132 1098.67 92.3348C1159.67 127.833 1221.12 167.38 1281.78 187.813C1342.78 207.869 1404.23 207.869 1464.89 192.803C1525.89 177.738 1587.34 147.607 1617.1 132.541L1648 117.475V238H1617.44C1587 238 1525.54 238 1464.89 238C1403.89 238 1342.43 238 1281.78 238C1220.78 238 1159.32 238 1098.67 238C1037.67 238 976.211 238 915.556 238C854.557 238 793.1 238 732.444 238C671.446 238 609.989 238 549.333 238C488.334 238 426.878 238 366.222 238C305.223 238 243.767 238 183.111 238C122.112 238 60.6556 238 30.9 238H0V177.738Z" fill="#262DDA"/>
-</svg>
-
-    </footer>
     <script src="https://kit.fontawesome.com/0e715a9c13.js" crossorigin="anonymous"></script>
     <script src="javascript.js"></script>
 </body>
